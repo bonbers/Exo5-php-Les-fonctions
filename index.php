d78df7c1a681b38bff5b71e5ad1106586beff484
bonbers <guillot.romain@ymail.com>
2017-04-21 12:03:40 +0200

    Exo en cours

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+   <head>
+     <meta charset="utf-8">
+     <title>Exo 5 PHP Les fonctions</title>
+   </head>
+
+   <body>
+
+ <p><?php
+
+function perso($var1, $var2){
+  if ($var1 < $var2){
+    echo "true";
+  }
+  else{
+    echo "false";
+  }
+}
+perso(15, 20);
+  ?></p>
+
+  <p><?php
+
+ function perso()
+
+   ?></p>
+
+  </body>
+
+</html>
